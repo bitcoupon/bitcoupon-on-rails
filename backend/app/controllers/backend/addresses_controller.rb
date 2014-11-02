ba88cf46688da2a605ab09dbaea1123d5144438a
Kristian Våge <krisvaage@gmail.com>
2014-11-02 16:29:52 +0100

    CSRF not applicable for API

--- backend/app/controllers/backend/addresses_controller.rb
+++ backend/app/controllers/backend/addresses_controller.rb
@@ -3,7 +3,7 @@ module Backend
   class AddressesController < ApplicationController
     before_action :check_headers, only: [:address, :word]
 
-    skip_before_filter :verify_authenticity_token, only: [:address]
+    skip_before_filter :verify_authenticity_token, only: [:address, :word]
 
     def address
       address = Address.where(address: params[:address].chomp)
