1f2542a70d461818598536b9a0ae443a936dcbb8
Kristian Våge <krisvaage@gmail.com>
2014-11-11 13:38:33 +0100

    Fix admin tests and remove unnecessary

--- admin/test/integration/api_test.rb
+++ admin/test/integration/api_test.rb
@@ -3,21 +3,18 @@ require 'test_helper'
 # API Test
 class ApiTest < ActionDispatch::IntegrationTest
   setup do
-    @api = 'http://localhost:3002/backend/creator_addresses'
-    @pubkey = 'sdflkj3209ikldjf23kljsd'
+    @api = 'http://localhost:3002/backend'
   end
 
   test 'should get correct response from api' do
     uri = URI.parse(@api)
     req = Net::HTTP::Get.new(uri)
 
-    req.add_field 'token', @pubkey
-
     result = Net::HTTP.start(uri.hostname, uri.port) do |http|
       http.request(req)
     end
 
     assert result.code.to_i.eql? 200
-    refute result.body.nil?
+    assert result.body.eql? ''
   end
 end
