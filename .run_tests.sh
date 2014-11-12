6da023cad0c4a7e89f5b3bcc165193e8bdee1882
Kristian Våge <krisvaage@gmail.com>
2014-11-12 16:25:54 +0100

    Update readme, delete two files and fix test runner bug

--- .run_tests.sh
+++ .run_tests.sh
@@ -1,7 +1,7 @@
 cd backend
 bundle exec rake db:migrate
-bundle exec bin/rails s -p 3002 &
-pid=$!
+bundle exec bin/rails s -p 3002 > /dev/null 2>&1 &
+PID=$!
 sleep 10
 cd ../admin
 echo -e "\n\nADMIN\n\n"
